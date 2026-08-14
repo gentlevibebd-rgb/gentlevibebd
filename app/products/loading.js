08bd01aad334d8e85a061127418b7d173079f0fb
gentlevibebd-rgb <gentlevibebd@gmail.com>
2026-08-14 19:41:23 +0600

    Added Our Shop instant loading screen and ISR performance optimizations

---
+++ app/products/loading.js
@@ -0,0 +1,19 @@
+import "./products.css";
+
+export default function ProductsLoading() {
+  return (
+    <>
+      {/* Top 3px Glowing Line */}
+      <div className="top-progress-line" />
+
+      {/* Centered Our Shop Loading Text */}
+      <div className="shop-loading-center">
+        <span className="shop-loading-tag">Gentle Vibe BD</span>
+        <h2 className="shop-loading-title">Our <em>Shop</em></h2>
+        <div className="shop-loading-dots">
+          <span></span><span></span><span></span>
+        </div>
+      </div>
+    </>
+  );
+}
